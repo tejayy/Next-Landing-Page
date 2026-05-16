fdc6047c4e920af22b48c83070a0d6df654829c7
tejayy <tbj456@gmail.com>
2026-05-16 20:02:33 +0530

    Added impact

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
+import { FeaturesSection } from "@/components/sections/features-section";
 import { HeroSection } from "@/components/sections/hero-section";
+import { ImpactSection } from "@/components/sections/impact-section";
+import { PricingSection } from "@/components/sections/pricing-section";
+import { TestimonialsSection } from "@/components/sections/testimonials-section";
 import { Navbar } from "@/components/ui/navbar";
+
 
 
 export default function Home() {
@@ -7,6 +12,10 @@ export default function Home() {
     <main className="min-h-screen bg-zinc-950">
       <Navbar />
       <HeroSection/>
+      <ImpactSection/>
+      <FeaturesSection/>
+      <TestimonialsSection/>
+      <PricingSection/>
       <h1 className="text-bold p-6">WELCOME</h1>
     </main>
   );
