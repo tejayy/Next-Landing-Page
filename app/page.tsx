b8fd999c353761d536a613b29350626493c4e4a9
tejayy <tbj456@gmail.com>
2026-05-16 19:50:32 +0530

    Added navabr and hero section components

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
+import { HeroSection } from "@/components/sections/hero-section";
+import { Navbar } from "@/components/ui/navbar";
+
+
 export default function Home() {
   return (
     <main className="min-h-screen bg-zinc-950">
+      <Navbar />
+      <HeroSection/>
       <h1 className="text-bold p-6">WELCOME</h1>
     </main>
   );
